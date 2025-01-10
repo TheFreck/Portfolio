d3551a2fb48620af665f0c6cea4228c2086a9ec4
theFreck <wpicaco@gmail.com>
2025-01-09 17:59:56 -0700

    initial

---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import { Paper } from "@mui/material";
+import react from "react";
+
+export const Projects = () => {
+
+    return <Paper>
+        Projects
+    </Paper>
+}
+
+export default Projects;
---
+++ src/components/ReadingList.jsx
@@ -0,0 +1,11 @@
+import { Paper } from "@mui/material";
+import react from "react";
+
+export const ReadingList = () => {
+
+    return <Paper>
+        Reading List
+    </Paper>
+}
+
+export default ReadingList;
